3e0833015051283ebefdf55de8185ad4442d810b
Shubhansu kumar <shubhansu2021@gmail.com>
2022-10-03 23:24:48 +0530

    Trello v3
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 00_JavaScript/Trello/02_TrelloV3.js
@@ -0,0 +1,67 @@
+// Trello V2 
+
+let todos = {
+    day: 'Mon',
+    meetings: 0,
+    meetDone: 0, 
+    
+    addMeeting: function(meet = 0){
+        this.meetings += meet
+        console.log(`Meetings: ${this.meetings}`)
+        console.log(this)
+        // console.log(todos)
+    },
+
+    completeMeeting : function(){
+        if (this.meetings){
+            this.meetDone++
+            this.meetings--
+            console.log(`Meetings: ${this.meetings}, MeetingsDone: ${this.meetDone}`)
+        }
+        else {
+            console.log('No meetings lined up')
+        }
+    },
+
+    reset: function(){
+        console.log('All meetings reset')
+        this.meetings = 0
+        this.meetDone = 0
+    }
+}
+
+// IF the function is inside the object, it's called a method 
+todos.addMeeting(5)
+todos.completeMeeting()
+
+// Assignment: 
+// Handle Meetings Done
+// Reset the entire day meet
+
+todos.completeMeeting()
+todos.reset()
+
+// Confusing Behavior: How does this works 
+let app = {
+    name: 'shubh',
+    func: function(){
+        // this is pointing to the obj 
+        console.log(this) // logs obj
+    }
+}
+let app1 = {
+    name: 'shubh',
+    func: ()=>{
+        // Using arrow function: this is not pointing to the obj
+        console.log(this) // logs obj 
+    }
+}
+
+app.func()
+app1.func()
+
+// Output:
+// { name: 'shubh', func: [Function: func] }
+// {}
+
+
